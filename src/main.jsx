303c5edcd0daef5e693d510681a22c4cf3011c0f
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-17 13:18:07 +0800

    clean up

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./pages/Root";
+import Home from "./pages/Home/Home";
+import Issue from "./pages/Issue/Issue";
+import Browse from "./pages/Browse/Browse";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "issues",
+        element: <Home />,
+      },
+      {
+        path: "/:query",
+        element: <Browse />,
+      },
+      {
+        path: "/:query/:page",
+        element: <Browse />,
+      },
+      {
+        path: "issues/:issueId",
+        element: <Issue />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
