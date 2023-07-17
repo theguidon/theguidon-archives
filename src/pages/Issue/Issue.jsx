6cc58acf2d9583c53093634e052af865b65c4c85
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-17 14:12:47 +0800

    sync up

---
+++ src/pages/Issue/Issue.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.vite";
+import pdf from "./pdf/iscs.pdf";
+
+export default function Issue() {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
+  const goToPrevPage = () =>
+    setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
+
+  const goToNextPage = () =>
+    setPageNumber(pageNumber + 1 >= numPages ? numPages : pageNumber + 1);
+
+  return (
+    <div>
+      <nav>
+        <button onClick={goToPrevPage}>Prev</button>
+        <button onClick={goToNextPage}>Next</button>
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
+      </nav>
+
+      <div className=" flex flex-row ">
+        {/* <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber} renderTextLayer={false} />
+        </Document>
+        <Document file={pdf} onLoadSuccess={onDocumentLoadSuccess}>
+          <Page pageNumber={pageNumber + 1} renderTextLayer={false} />
+        </Document> */}
+      </div>
+    </div>
+  );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        guidon: "#1C4480",
+      },
       fontFamily: {
-        chivo: ['"Chivo"', ...defaultTheme.fontFamily.sans]
-      }
+        chivo: ["Chivo", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
