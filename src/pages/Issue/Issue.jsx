08023a3c3ed90163ea153639f75c76c78d8bf553
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-19 00:17:00 +0800

    zoom pan pinch

--- src/pages/Issue/Issue.jsx
+++ src/pages/Issue/Issue.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import pdf from "./test.pdf";
+
+function IssueViewer({ file }) {
+  return (
+    <TransformWrapper className="h-[30rem]">
+      <TransformComponent>
+        <Document file={file}>
+          <Page pageNumber={1} height={480} renderAnnotationLayer={false} />
+        </Document>
+      </TransformComponent>
+    </TransformWrapper>
+  );
+}
 
 export default function Issue() {
   return (
-    <main className="flex flex-col justify-center items-center">
-      <Document file={pdf}>
-        <Page pageNumber={1} width={400} renderAnnotationLayer={false} />
-      </Document>
+    <main>
+      <IssueViewer file={pdf} />
     </main>
   );
 }
