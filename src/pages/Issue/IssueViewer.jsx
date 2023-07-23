01b2e3f67edb5fd788a0304c899d74d137639014
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-24 01:21:36 +0800

    issue drag

--- src/pages/Issue/IssueViewer.jsx
+++ src/pages/Issue/IssueViewer.jsx
@@ -1,13 +1,16 @@
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 import { useGesture } from "@use-gesture/react";
 import { useRef } from "react";
+import { gsap } from "gsap";
 
 export default function IssueViewer({ file }) {
   const documentRef = useRef(null);
 
   useGesture(
     {
-      onDrag: (state) => {},
+      onDrag: ({ offset: [x, y] }) => {
+        gsap.to(documentRef.current, { x, y });
+      },
     },
     {
       target: documentRef,
@@ -15,7 +18,7 @@ export default function IssueViewer({ file }) {
   );
 
   return (
-    <div className="h-[30rem] flex flex-row justify-center items-center">
+    <div className="h-[30rem] flex flex-row justify-center items-center border-solid border-red-500 border-2">
       <Document
         file={file}
         inputRef={documentRef}
