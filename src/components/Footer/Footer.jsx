edd28c5ec193dd1052e889cd60530f2a29f40159
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-25 13:17:59 +0800

    setup stylesheets

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./styles.css";
 import logo from "../../assets/images/logo-blue.svg";
 import facebook from "../../assets/icons/facebook.svg";
 import twitter from "../../assets/icons/twitter.svg";
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./styles.css";
 import logo from "../../assets/images/logo-white.svg";
 import menu from "../../assets/icons/menu.svg";
 import { Link } from "react-router-dom";
@@ -73,34 +74,6 @@ const Header = () => {
         <div className="w-screen h-16" />
       </header>
     </>
-    // <header className="w-full fixed flex justify-center items-center bg-blue-900 px-12 py-3 text-white font-chivo text-s z-50">
-    //   <div className="flex w-full justify-between">
-    //     <div className="flex items-center pl-12 gap-1.5">
-    //       <img className="" src={logo} alt="The GUIDON" />
-    //     </div>
-    //     <div className="flex pr-12 gap-6">
-    //       <ul className="flex items-center">
-    // <Link to="/">
-    //   <li className="text-white mx-6 font-bold">HOME</li>
-    // </Link>
-    // <Link to="/about">
-    //   <li className="text-white mx-6 font-bold">ABOUT</li>
-    // </Link>
-    // <Link to="/releases">
-    //   <li className="text-white mx-6 font-bold">RELEASES</li>
-    // </Link>
-    //       </ul>
-    //       <form className="flex justify-between text-black items-center px-4 py-1.5 bg-white rounded-lg">
-    //         <input
-    //           className="focus:outline-none bg-white"
-    //           type="text"
-    //           placeholder="Search"
-    //         />
-    //         <img src={search} alt="search" />
-    //       </form>
-    //     </div>
-    //   </div>
-    // </header>
   );
 };
 
