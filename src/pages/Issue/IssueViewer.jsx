73e4879d62faefde4d002536dcaea2d317a1487e
Waleed Lugod <lugodwaleed@gmail.com>
2023-07-25 12:43:44 +0800

    scroller

--- src/pages/Issue/IssueViewer.jsx
+++ src/pages/Issue/IssueViewer.jsx
@@ -1,6 +1,6 @@
 import { Document, Page } from "react-pdf/dist/esm/entry.vite";
 import { useDrag, useGesture } from "@use-gesture/react";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import arrowLeft from "../../assets/icons/arrow-left.svg";
 
@@ -9,6 +9,7 @@ export default function IssueViewer({ file }) {
   const documentRef = useRef(null);
   const scrollRef = useRef(null);
   const thumbRef = useRef(null);
+  const trackRef = useRef(null);
   const [pageNumber, setPageNumber] = useState(0);
   const [numPages, setNumPages] = useState(0);
 
@@ -27,13 +28,24 @@ export default function IssueViewer({ file }) {
     }
   );
 
+  function handleScrollThumb(x) {
+    const scrollWidth = scrollRef.current.getBoundingClientRect().width - 1;
+    const xPos = gsap.utils.snap(scrollWidth / numPages - 1, x);
+    gsap.set(thumbRef.current, { x: xPos });
+    gsap.set(trackRef.current, { width: xPos + 20 });
+  }
+
   // scroller gestures
-  useDrag(
-    ({ offset: [x] }) => {
-      gsap.set(thumbRef.current, { x });
-    },
-    { target: thumbRef, bounds: scrollRef }
-  );
+  useDrag(({ offset: [x] }) => handleScrollThumb(x), {
+    target: thumbRef,
+    bounds: scrollRef,
+  });
+
+  useEffect(() => {
+    const scrollWidth = scrollRef.current.getBoundingClientRect().width - 1;
+    const pageScale = scrollWidth / numPages - 1;
+    handleScrollThumb(pageNumber * pageScale);
+  }, [pageNumber]);
 
   return (
     <div>
@@ -82,14 +94,18 @@ export default function IssueViewer({ file }) {
           )}
         </Document>
       </div>
-      <div className="w-full h-24 bg-[#DBE9F4] relative z-10 flex flex-row font-chivo text-[#666] items-center justify-center gap-x-5">
+      <div className="w-full h-24 bg-[#DBE9F4] flex flex-row font-chivo text-[#666] items-center justify-center gap-x-5">
         <div
           ref={scrollRef}
           className="w-[70rem] h-4 rounded-[0.675rem] border-[#B6C2CD] border-[1px] bg-white relative"
         >
           <div
+            ref={trackRef}
+            className="absolute left-0 top-0 bottom-0 w-0 bg-guidon rounded-[inherit]"
+          />
+          <div
             ref={thumbRef}
-            className="absolute top-1/2 -translate-y-1/2 rounded-full w-5 aspect-square border-[#6A757C] border-[1px] bg-[#E9EEF2] cursor-pointer"
+            className="absolute top-1/2 -translate-y-1/2 rounded-full w-5 aspect-square border-[#6A757C] border-[1px] bg-[#E9EEF2] cursor-pointer touch-none"
           />
         </div>
         <p>{`Page${pageNumber > 0 && pageNumber < numPages ? "s" : ""} ${
