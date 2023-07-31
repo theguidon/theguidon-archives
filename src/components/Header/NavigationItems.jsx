007313a0c0df2af5b5b728f026533fe30f1141c3
Bastinoroni96 <gsbasti96@gmail.com>
2023-07-31 18:16:07 +0900

    fixed padding and hover, adjusted select image of "Releases" button"

--- src/components/Header/NavigationItems.jsx
+++ src/components/Header/NavigationItems.jsx
@@ -4,18 +4,18 @@ import select from "../../assets/icons/selection.svg";
 
 const NavigationItems = () => {
   return (
-    <div className="flex items-center ml-8">
+    <div className="flex items-center ml-8 nav-container">
       {/* Nav Home */}
       <Link
         to="/"
         type="button"
-        className="flex items-center px-2 py-8 rounded-16 bg-blue-the-guidon-blue font-bold mr-2 nav-button"
+        className="flex items-center px-4 py-2 rounded-16 bg-blue-the-guidon-blue font-bold mr-2 nav-button"
       >
         Home
       </Link>
       {/* Nav Release (DROPDOWN SELECTION) */}
       <button
-        className="relative flex items-center px-2 py-8 rounded-16 bg-blue-the-guidon-blue font-bold mr-2 nav-button"
+        className="relative flex items-center px-4 py-2 rounded-16 bg-blue-the-guidon-blue font-bold mr-2 nav-dropdown"
         onMouseEnter={() => {
           console.log("Releases Open");
           // Add code to show/hide the overlay and handle the animation if needed
@@ -25,13 +25,13 @@ const NavigationItems = () => {
         <img
           src={select}
           alt="Selection Icon"
-          className="w-5 h-5" // Adjust width and height as needed
+          className="w-5 h-5 ml-2" // Adjust width and height as needed
         />
       </button>
       {/* ABOUT */}
       <Link
         to="/about"
-        className="relative flex items-center px-2 py-8 rounded-16 bg-blue-the-guidon-blue flex-container font-bold nav-button"
+        className="relative flex items-center px-4 py-2 rounded-16 bg-blue-the-guidon-blue flex-container font-bold nav-button"
         onMouseEnter={() => {
           console.log("About Button Hovered");
           // Add code to show/hide the overlay or any other actions on hover
