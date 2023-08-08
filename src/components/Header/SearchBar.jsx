b11c43ba356264411079c5ae31b25af727c64786
Waleed Lugod <lugodwaleed@gmail.com>
2023-08-08 16:15:25 +0800

    respnsive

--- src/components/Header/SearchBar.jsx
+++ src/components/Header/SearchBar.jsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
   const [search, toggleSearch] = useState(false);
 
   return (
-    <div className="relative max-w-[34rem] w-full">
+    <div className="relative max-w-[34rem] min-w-[23rem] w-full">
       <svg
         viewBox="0 0 17 18"
         width="17"
