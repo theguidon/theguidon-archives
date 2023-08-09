0e5c124ed30330e3f0c2663d3f5f9c5fc869573e
Waleed Lugod <lugodwaleed@gmail.com>
2023-08-09 20:13:37 +0800

    new layout

--- src/pages/Issue/Issue.jsx
+++ src/pages/Issue/Issue.jsx
@@ -10,14 +10,14 @@ export default function Issue() {
   return (
     <main>
       <IssueViewer file={pdf} />
-      <div className=" w-screen flex flex-row flex-shrink-0 justify-center items-center p-6 sm:p-16 lg:p-24 gap-x-6 lg:gap-x-20">
-        <div className="relative w-40 lg:w-[18rem] sm:w-[13rem] flex-shrink-0  aspect-square bg-[#EFF5FA]">
+      <div className="w-screen flex flex-col-reverse lg:flex-row flex-shrink-0 justify-center items-center p-6 sm:p-16 lg:p-24 gap-x-6 lg:gap-x-20 gap-y-4">
+        <div className="relative w-full max-w-[30rem] lg:max-w-none lg:w-[18rem] flex-shrink-0 h-[15rem] sm:h-[20rem] lg:h-auto lg:aspect-square bg-[#EFF5FA]">
           <img
             src={image}
             className=" max-w-[80%] max-h-[80%] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
           />
         </div>
-        <div className="max-w-[50%]">
+        <div className="lg:max-w-[50%] max-w-[30rem]">
           <div>
             <h1 className="lg:text-[2.5rem] text-lg sm:text-3xl font-tiemposheadline text-guidon font-bold leading-[1.4]">
               FreshManual 2023
@@ -25,7 +25,7 @@ export default function Issue() {
             <p className="text-[#979797] text-xs lg:text-base font-chivo uppercase tracking-[0.04rem]">
               7 August 2023
             </p>
-            <p className="font-chivo text-guidon text-xs sm:text-base lg:text-xl mt-2 mb-8">
+            <p className="font-chivo text-sm sm:text-base lg:text-xl mt-2 mb-8">
               Getting ready for college just got way better. Prep for the year
               ahead with Vantage Magazine's Freshmanual 2023! This year's
               edition is full of tips and tricks from upperclassmen—from
