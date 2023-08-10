5a6f30c1d6a8bac5d5ba6877129a33d1f64c7805
Waleed Lugod <lugodwaleed@gmail.com>
2023-08-10 18:37:52 +0800

    setup about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "./styles.css";
+import styles from "./About.module.css";
 
 export default function About() {
-  return <main>about</main>;
+  return <main className={styles.test}>about</main>;
 }
