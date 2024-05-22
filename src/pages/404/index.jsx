ae5615f666459d8ba383fbe6395cd6c7d04ea3f9
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-23 07:25:48 +0800

    add 404 content

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,7 +1,22 @@
 import "./index.css";
 
 function Page404() {
-  return <div>404</div>;
+  return (
+    <div id="page-404" className="general-container">
+      <p className="subheader">We couldn't find any matches for</p>
+      <h2>“What is the best GDN Staff?”</h2>
+      <hr />
+
+      <ul>
+        <li>Double-check the spelling or try using different keywords.</li>
+        <li>Broaden your search query to include more general terms.</li>
+        <li>
+          Try refining your search with specific filters to narrow down the
+          results.
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Page404;
