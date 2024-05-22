a77b0645e4716acea15f0a1451db0b7a6e96a809
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-23 04:21:56 +0800

    finish search field

--- src/components/search-field/index.jsx
+++ src/components/search-field/index.jsx
@@ -1,7 +1,37 @@
 import "./index.css";
 
+import searchIcon from "./../../assets/icons/search.svg";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+
 function SearchField() {
-  return <form>SEARCH</form>;
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    navigate(`/search?query=${encodeURIComponent(query)}`);
+  };
+
+  useEffect(() => {
+    let q = searchParams.get("query");
+
+    if (q) setQuery(searchParams.get("query"));
+  }, [searchParams.get("query")]);
+
+  return (
+    <form className="search-field" onSubmit={onSubmit}>
+      <img src={searchIcon} />
+      <input
+        type="text"
+        name="query"
+        placeholder="Find a press issue, magazine, primer, etc."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
 }
 
 export default SearchField;
