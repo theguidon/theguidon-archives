e4e12eee38c1b9d88b029bcc2c9e310ec8124120
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-24 00:18:45 +0800

    finish 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -1,9 +1,28 @@
+import IssueCard from "../../components/issue-card";
 import "./index.css";
 
 function Page404() {
   return (
     <div id="page-404" className="general-container">
-      404
+      <p className="subheader">Error 404</p>
+      <h2>Page not found.</h2>
+      <hr />
+
+      <p className="text">
+        Try searching for it again. It may have been removed or there might have
+        been a typo in the URL.
+      </p>
+
+      <p className="subheader">Our latest releases</p>
+      <h4>You might be interested in these instead.</h4>
+
+      <div className="card-grid">
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+      </div>
     </div>
   );
 }
