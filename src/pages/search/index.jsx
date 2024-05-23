7ebbc1a534190b8a03b72d59ed4c8ecb7d21a94e
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-24 00:09:39 +0800

    move original 404 to search results

--- src/pages/search/index.jsx
+++ src/pages/search/index.jsx
@@ -1,7 +1,22 @@
 import "./index.css";
 
 function SearchPage() {
-  return <div>SEARCH PAGE</div>;
+  return (
+    <div id="search-results" className="general-container">
+      <p className="subheader">We couldn't find any matches for</p>
+      <h2>“What is the best GDN Staff?”</h2>
+      <hr />
+
+      <ul>
+        <li>Double-check the spelling or try using different keywords.</li>
+        <li>Broaden your search query to include more general terms.</li>
+        <li>
+          Try refining your search with specific filters to narrow down the
+          results.
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default SearchPage;
