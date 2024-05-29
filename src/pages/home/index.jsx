d034a1b8411d01d627168cbf06815ac73f1e3a17
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-05-30 04:54:01 +0800

    replace img with svg, respo

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,7 +2,6 @@ import IssueCard from "../../components/issue-card";
 import "./index.css";
 
 import img from "./../../assets/images/broadsheet-sample.png";
-import chevronRight from "./../../assets/icons/chevron-right.svg";
 import { Link } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -62,6 +61,15 @@ function HomePage() {
     },
   ];
 
+  const chevronRight = (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 40 40" fill="none">
+      <path
+        d="M14.5786 26.8126C13.9635 27.369 13.9146 28.32 14.4695 28.9367C15.0244 29.5535 15.9729 29.6025 16.588 29.0461L25.4248 21.0541C26.0867 20.4554 26.0849 19.4134 25.4209 18.8171L16.6613 10.9504C16.0442 10.3962 15.0959 10.4485 14.5432 11.0672C13.9904 11.6859 14.0426 12.6367 14.6597 13.1909L21.846 19.6447C22.0226 19.8033 22.0231 20.0798 21.8471 20.239L14.5786 26.8126Z"
+        fill="#1C4480"
+      />
+    </svg>
+  );
+
   return (
     <div id="home">
       {issues.data.all != null && issues.data.all[0] != null && (
@@ -103,7 +111,7 @@ function HomePage() {
         <p className="subheader">Recently Uploaded</p>
         <Link to="releases/this-term" className="row">
           <h3>New on the Archive</h3>
-          <img src={chevronRight} />
+          {chevronRight}
         </Link>
         <hr />
         <div id="latest" className="card-grid mobile-list">
@@ -120,7 +128,7 @@ function HomePage() {
         <p className="subheader">Browse</p>
         <Link to="/releases/recent" className="row">
           <h3>The Archive</h3>
-          <img src={chevronRight} />
+          {chevronRight}
         </Link>
         <hr />
         <div id="recently-uploaded" className="card-grid mobile-list">
@@ -128,7 +136,16 @@ function HomePage() {
             <Link to={categ.link} key={`categ-${idx}`} className="categ-card">
               <div className="row">
                 <h5>{categ.title}</h5>
-                <img src={chevronRight} />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 25"
+                  fill="none"
+                >
+                  <path
+                    d="M8.7472 16.5876C8.37811 16.9214 8.3488 17.492 8.68173 17.862C9.01466 18.2321 9.58376 18.2615 9.95284 17.9277L15.2549 13.1324C15.6521 12.7732 15.651 12.1481 15.2525 11.7902L9.99678 7.07022C9.62653 6.73771 9.05755 6.7691 8.72591 7.14032C8.39428 7.51154 8.42558 8.08202 8.79582 8.41453L12.9755 12.1682C13.1521 12.3268 13.1526 12.6033 12.9766 12.7625L8.7472 16.5876Z"
+                    fill="#1C4480"
+                  />
+                </svg>
               </div>
               <hr />
               <div className="cover-container">
@@ -148,7 +165,7 @@ function HomePage() {
         <p className="subheader">Explore</p>
         <Link to="/releases" className="row">
           <h3>History</h3>
-          <img src={chevronRight} />
+          {chevronRight}
         </Link>
         <hr />
         <div id="history" className="card-grid mobile-list">
