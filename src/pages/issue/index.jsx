eb59e9eb73a6bdf07ddf30b47d14e375667065dd
Emman Evangelista <emmanuel.evangelista@student.ateneo.edu>
2024-06-13 22:56:29 +0800

    fix sharer urls

--- src/pages/issue/index.jsx
+++ src/pages/issue/index.jsx
@@ -216,7 +216,8 @@ function IssuePage() {
               </Link>
 
               <Link
-                to={`http://x.com/share?url=https://issues.theguidon.com/issue/${issue.fixed_slug}&text=${issue.title}`}
+                // to={`http://x.com/share?url=https://archives.theguidon.com/issue/${issue.fixed_slug}&text=View the ${issue.title} Release on The GUIDON Archives: https://archives.theguidon.com/issue/${issue.fixed_slug}`}
+                to={`http://x.com/share?text=View the ${issue.title} release on The GUIDON Archives: https://archives.theguidon.com/issue/${issue.fixed_slug}`}
                 target="_blank"
               >
                 <svg
@@ -227,61 +228,6 @@ function IssuePage() {
                   <path d="M12.8998 11.1983L19.0923 4H17.6249L12.2479 10.2502L7.9533 4H3L9.49426 13.4514L3 21H4.46752L10.1458 14.3996L14.6812 21H19.6345L12.8994 11.1983H12.8998ZM10.8898 13.5347L10.2318 12.5936L4.99629 5.10473H7.25031L11.4754 11.1485L12.1334 12.0896L17.6256 19.9455H15.3715L10.8898 13.5351V13.5347Z" />
                 </svg>
               </Link>
-
-              {/* <svg
-                // width="24"
-                // height="24"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-                onClick={async () => {
-                  const canvas = document.createElement("canvas");
-                  canvas.width = 1080;
-                  canvas.height = 1920;
-
-                  const ctx = canvas.getContext("2d");
-                  ctx.fillStyle = "red";
-                  ctx.fillRect(0, 0, 150, 150);
-
-                  const cover = new Image();
-                  cover.src = issue.cover_full;
-                  cover.crossOrigin = "anonymous";
-
-                  cover.onload = () => {
-                    ctx.drawImage(cover, 0, 0);
-
-                    canvas.toBlob((blob) => {
-                      let data = {
-                        files: [
-                          new File([blob], "file.png", {
-                            type: blob.type,
-                          }),
-                        ],
-                        title: issue.title,
-                        text: issue.fixed_slug,
-                        url: "https://dev.theguidon.com",
-                      };
-
-                      if (
-                        window.navigator.canShare &&
-                        window.navigator.canShare(data)
-                      )
-                        window.navigator.share(data);
-                    });
-                  };
-                }}
-              >
-                <path
-                  d="M12 8.75C11.138 8.75 10.3114 9.09241 9.7019 9.7019C9.09241 10.3114 8.75 11.138 8.75 12C8.75 12.862 9.09241 13.6886 9.7019 14.2981C10.3114 14.9076 11.138 15.25 12 15.25C12.862 15.25 13.6886 14.9076 14.2981 14.2981C14.9076 13.6886 15.25 12.862 15.25 12C15.25 11.138 14.9076 10.3114 14.2981 9.7019C13.6886 9.09241 12.862 8.75 12 8.75Z"
-                  // fill="white"
-                />
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M6.77 3.082C10.246 2.6967 13.754 2.6967 17.23 3.082C19.129 3.294 20.66 4.789 20.883 6.695C21.2952 10.2197 21.2952 13.7803 20.883 17.305C20.66 19.211 19.129 20.706 17.231 20.919C13.7547 21.3044 10.2464 21.3044 6.77 20.919C4.871 20.706 3.34 19.211 3.117 17.306C2.70473 13.781 2.70473 10.22 3.117 6.695C3.34 4.789 4.871 3.294 6.77 3.082ZM17 6C16.7348 6 16.4804 6.10536 16.2929 6.2929C16.1054 6.48043 16 6.73479 16 7C16 7.26522 16.1054 7.51957 16.2929 7.70711C16.4804 7.89465 16.7348 8 17 8C17.2652 8 17.5196 7.89465 17.7071 7.70711C17.8946 7.51957 18 7.26522 18 7C18 6.73479 17.8946 6.48043 17.7071 6.2929C17.5196 6.10536 17.2652 6 17 6ZM7.25 12C7.25 10.7402 7.75045 9.53205 8.64125 8.64125C9.53205 7.75045 10.7402 7.25 12 7.25C13.2598 7.25 14.468 7.75045 15.3588 8.64125C16.2496 9.53205 16.75 10.7402 16.75 12C16.75 13.2598 16.2496 14.468 15.3588 15.3588C14.468 16.2496 13.2598 16.75 12 16.75C10.7402 16.75 9.53205 16.2496 8.64125 15.3588C7.75045 14.468 7.25 13.2598 7.25 12Z"
-                  // fill="white"
-                />
-              </svg> */}
             </div>
           </div>
         ) : (
